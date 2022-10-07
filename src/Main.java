184d9163ebd5d5b3f98a9555f0239e7d60d9c30d
gulyolik <olia.guliaeva@yandex.ru>
2022-10-07 19:53:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price;
+        price = 100;
+        int bonus = price / 20;
+        System.out.println(bonus + "миль");
+    }
+
+}
